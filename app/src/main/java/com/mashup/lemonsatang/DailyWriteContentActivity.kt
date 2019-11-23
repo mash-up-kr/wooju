8c57e673c9e607a94d3f1b4c7e49698c175537ee
kwonsye <kwonsye@naver.com>
2019-11-24 02:57:48 +0900

    add dialog

--- app/src/main/java/com/mashup/lemonsatang/DailyWriteContentActivity.kt
+++ app/src/main/java/com/mashup/lemonsatang/DailyWriteContentActivity.kt
@@ -2,11 +2,21 @@ package com.mashup.lemonsatang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
+import kotlinx.android.synthetic.main.activity_daily_write_content.*
 
 class DailyWriteContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_write_content)
+
+        tv_submit.setOnClickListener {
+            val builder = MaterialDialog(this).show{
+                message (text = "Dialog")
+                positiveButton (text="작성 완료되었습니다.")
+            }
+        }
+
     }
 }
