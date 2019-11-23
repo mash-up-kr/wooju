2f3611d2077ac484c44153bafc5228b94bd384d4
harry5991 <harry5991@cleaninglab.co.kr>
2019-11-24 00:42:17 +0900

    #4 액티비티 애니메이션 만드는 중....

--- app/src/main/java/com/mashup/lemonsatang/DailyWriteActivity.kt
+++ app/src/main/java/com/mashup/lemonsatang/DailyWriteActivity.kt
@@ -1,12 +1,28 @@
 package com.mashup.lemonsatang
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_daily_write.*
+
 
 class DailyWriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_write)
+
+        et_daily_content.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                val intent = Intent(this, DailyWriteContentActivity::class.java)
+                startActivity(intent);
+                overridePendingTransition(R.anim.rightin_activity, R.anim.not_move_activity);
+
+                overridePendingTransition(R.anim.not_move_activity, R.anim.rightout_activity);
+
+                overridePendingTransition(R.anim.slide_down, R.anim.slide_up)
+                et_daily_content.clearFocus()
+            }
+        }
     }
 }
