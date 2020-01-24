a1e84900eae71ec80af4775933387ac7cac759bf
godjoy <tlschgml556@gmail.com>
2020-01-24 17:21:26 +0900

    add isBlank to companion object

--- app/src/main/java/com/mashup/lemonsatang/ui/vo/RemindListItemVo.kt
+++ app/src/main/java/com/mashup/lemonsatang/ui/vo/RemindListItemVo.kt
@@ -3,10 +3,25 @@ package com.mashup.lemonsatang.ui.vo
 import com.google.gson.annotations.SerializedName
 
 data class RemindListItemVo(
-    var month : String?,
+    var type : Int,
+    var month : String,
     var date : String?,
+    var contents : String?,
+    var emotionColor : Int?,
+    var start : String?
+)
+{
+    companion object{
+        const val itemType=0
+        const val monthType=1
+        const val blankType=2
+    }
+}
+
+
+data class RemindListResponse(
     @SerializedName("startDate") var start : String,
     @SerializedName("endDate") var end : String,
-    @SerializedName("command")var content : String,
+    @SerializedName("command")var contents : String,
     @SerializedName("bestEmotion") var emotionColor : Int
 )
