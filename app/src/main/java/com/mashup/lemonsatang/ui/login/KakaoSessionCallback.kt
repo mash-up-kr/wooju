eb17df048a81e375e6e3521357439ca0293d8c36
Taehwi Jeong <preludez.dev@gmail.com>
2020-01-31 21:00:31 +0900

    #9 로그인페이지
    - 로그인페이지 임시 완성

---
+++ app/src/main/java/com/mashup/lemonsatang/ui/login/KakaoSessionCallback.kt
@@ -0,0 +1,33 @@
+package com.mashup.lemonsatang.ui.login
+
+import android.util.Log
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.exception.KakaoException
+
+class KakaoSessionCallback(val redirect: () -> Unit) : ISessionCallback {
+    override fun onSessionOpenFailed(exception: KakaoException?) {
+        if (exception != null) {
+            Log.d("test", exception.toString())
+        }
+    }
+
+    override fun onSessionOpened() {
+        UserManagement.getInstance().me(object: MeV2ResponseCallback() {
+            override fun onSuccess(result: MeV2Response?) {
+                Log.d("test", "accessToken : ${Session.getCurrentSession().tokenInfo.accessToken}")
+                Log.d("test", "refreshToken : ${Session.getCurrentSession().tokenInfo.refreshToken}")
+            }
+
+            override fun onSessionClosed(errorResult: ErrorResult?) {
+                Log.d("test", "onSessionClosed.")
+            }
+        })
+
+        redirect()
+    }
+}
